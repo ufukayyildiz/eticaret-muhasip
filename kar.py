c1d0ae17ba4615f2b4bd705af018e87ddf30e721
Furkan Ünlütürk <unluturkfurkan@protonmail.com>
2021-07-25 01:29:05 +0300

    büyük değişiklikler

--- kar.py
+++ kar.py
@@ -1,13 +1,13 @@
 # "Bize ne bırakıyor/kalıyor?"un cevabını verir.
 # geliş + kar + KDV + kargo + komisyon = satış fiyatı
 
-kargo = 15
-satis_fiyati = float(input("Satış Fiyatı (TL): "))
-gelis = float(input("Geliş Fiyatı (TL): "))
 kdv_yuzdesi = float(input("KDV (%): "))
 komisyon_yuzdesi = float(input("Komisyon (%): "))
+gelis = float(input("Geliş Fiyatı (TL): "))
+kargo = float(input("Kargo Ücreti (TL): "))
+satis_fiyati = float(input("Kargo DAHİL Satış Fiyatı (TL): "))
 
-akk = (1 + kdv_yuzdesi/100)
+akk = 1 + kdv_yuzdesi/100
 
 kar = satis_fiyati - akk*gelis - kargo - satis_fiyati*komisyon_yuzdesi/100
 
--- satis_fiyati.py
+++ satis_fiyati.py
@@ -9,10 +9,10 @@
 
 # TEASS: Tavsiye Edilen Asgari Satış Fiyatı = (kargo/teaky)*100
 
-kargo = 15
+kdv_yuzdesi = float(input("KDV (%): "))
 komisyon_yuzdesi = float(input("Komisyon (%): "))
 gelis = float(input("Geliş Fiyatı (TL): "))
-kdv_yuzdesi = float(input("KDV (%): "))
+kargo = float(input("Kargo Ücreti (TL): "))
 akk = (kdv_yuzdesi/100 + 1)
 teaky = (100 - komisyon_yuzdesi)/(akk*gelis + 2*kargo)*kargo
 teass = (kargo/teaky)*100
@@ -20,7 +20,9 @@ print(f"Tavsiye Edilen Asgari Kar Yüzdesi: {round(teaky,2)}")
 kar_yuzdesi = float(input("Kar (%): "))
 
 k = (akk*gelis + kargo) / (100 - kar_yuzdesi - komisyon_yuzdesi)
+satis_fiyati = 100*k
 
-print(f"\nSatış Fiyatı (TL): \t\t {round(100*k,2)}")
-print(f"Kar (TL): \t\t\t {round(kar_yuzdesi*k,2)}")
-print(f"Tavsiye Edilen Asgari Satış Fiyatı: {round(teass,2)}")
+print(f"\nSatış Fiyatı:\t\t\t\t{round(satis_fiyati)} TL = {round((satis_fiyati - kargo),2)} TL + {kargo} TL")
+print(f"Kar:\t\t\t\t\t{round(kar_yuzdesi*k,2)} TL")
+print(f"\nTavsiye Edilen Asgari Satış Fiyatı:\t{round(teass,2)} TL = {round((teass - kargo),2)} TL + {kargo} TL")
+print(f"Kar:\t\t\t\t\t{round(teaky*k,2)} TL")
